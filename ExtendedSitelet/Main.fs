39fb9c997552db2109d4ee0ff48a2d6a01d0a672
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2013-12-21 09:47:19 -0800

    Some more updates to help me understand how websharper works

--- ExtendedSitelet/Main.fs
+++ ExtendedSitelet/Main.fs
@@ -20,6 +20,7 @@ module Controls =
         override __.Body =
             Client.Main() :> _
 
+    // I've written a client side control inside here but I guess really it should be in the client module
     open IntelliFactory.WebSharper.Html
     type MenuControl() =
         inherit Web.Control()
@@ -27,19 +28,21 @@ module Controls =
         override this.Body =
             I [Text "Some client stuff embedded in a control"; Attr.Style "color: green"] :> IPagelet
 
+    // ditto
     type ClockControl() =
         inherit Web.Control()
         let now = System.DateTime.Now.ToString()
         [<JavaScript>]
         override this.Body = 
             Div [
-                Span [Text "Current time: "]
+                Span [Text "Another client control this time embedding the current time: "]
                 Span [Text now ] 
             ] :> IPagelet
 
 module Skin =
     open System.Web
 
+    // let's make our templated page a little more complex
     type Page =
         {
             Title : string
@@ -59,7 +62,7 @@ module Skin =
         Content.WithTemplate MainTemplate <| fun context ->
             {
                 Title = title
-                Menu = [ Div [Text "test"] ] 
+                Menu = [ Div [Text "A bit of server side html combinator stuff embedded in code creating an instance of the page record"] ] // great I can just embed combinators here
                 Body = body context
                 Footer = footer context
             }
@@ -72,8 +75,8 @@ module Site =
     let Links (ctx: Context<Action>) =
         UL [
             LI ["Home" => ctx.Link Home]
-//            LI ["About" => ctx.Link About]
-//            LI ["Login" => ctx.Link Login]
+            LI ["About" => ctx.Link About]
+            LI ["Login" => ctx.Link Login]
         ]
 
     let HomePage =
@@ -85,32 +88,44 @@ module Site =
             ] )
             <| fun ctx ->
             [
-                Div [Text "Footer I guess..."]
-                Div [new Controls.ClockControl()]
+                // this is going to be footer stuff - rather than doing this every time this should really be a function shouldn't it...
+                Div [Text "This is a one off footer on the home page"]
             ]
 
+
+
+    let aFooter : Content.HtmlElement list =
+        // so I could also stick this out in another file I suppose
+        [
+            // this is going to be footer stuff - rather than doing this every time this should really be a function shouldn't it...
+            Div [Text "This is my footer with something appropriate to put at the bottom of every page... let's display the current date and time from our webcontrol"]
+            Div [new Controls.ClockControl()]
+        ]
+
     let AboutPage =
-        Skin.WithTemplate "AboutPage" <| fun ctx ->
+        Skin.WithTemplate "AboutPage" <| (fun ctx ->
                 [
+                Div [Text "About Page"]
                 Div [new Controls.MenuControl() ] -< [
                     P [Align "center"]  -<
                         [Text "Explain what this is all about"]
                 ]
                 Links ctx
-            ]
+            ] )
+            <| fun ctx -> aFooter
 
     let LoginPage =
-        Skin.WithTemplate "LoginPage" <| fun ctx ->
+        Skin.WithTemplate "LoginPage" <| (fun ctx ->
             [
-                Div [Text "LOGIN"]
+                Div [Text "Login Page"]
                 Links ctx
-            ]
+            ] ) <| fun ctx -> aFooter
 
     let Main =
         Sitelet.Sum [
             Sitelet.Content "/" Home HomePage
-//            Sitelet.Content "/About" About AboutPage
-//            Sitelet.Content "/Login" Login LoginPage
+            Sitelet.Content "/About" About AboutPage
+            Sitelet.Content "/Login" Login LoginPage
         ]
 
 [<Sealed>]
